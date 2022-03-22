cd9018b3d87f1ed9f241762df40998ca55c23407
Bagus Akmal <101700139+bagusakmal@users.noreply.github.com>
2022-03-22 17:21:23 +0700

    update studi kasus perrtamina ke 2

--- Kegiatan/TugasStudiKasusPertamina.cpp
+++ Kegiatan/TugasStudiKasusPertamina.cpp
@@ -97,3 +97,55 @@ void Bensin::hitung_uang(){
      system("cls");
      pilihan();
      }
+void Bensin::hitung_liter(){
+     cout<<"masukkan jumlah liter : ";
+     cin>> liter;
+      if(ambil == 'a'){
+              harga = pertalite;
+              }
+     else if (ambil == 'b'){
+          harga = pertamax;
+          }
+     tampil = harga * liter;
+     if(ambil == 'a'){
+     		cout << "\n=======Struk SPBU UAD=======" << endl;
+			cout << "Nama Pelanggan :  " << na << endl;
+			cout << "Total Liter \t: " << liter << endl;
+            cout << "Jenis Bensin \t: " << "Pertalite" << endl;
+            cout << "Total Harga \t: " << tampil << endl;
+            ofstream hsl;
+        	hsl.open("struct.txt", ios::out);
+        	hsl <<  "\n=======Struk SPBU UAD=======" << endl;
+        	hsl <<  "Nama Pelanggan : " << na << endl;
+        	hsl <<  "Total Liter \t: " << liter << endl;
+        	hsl <<  "Jenis Bensin \t: " << "Pertalite" << endl;
+        	hsl <<  "Total Harga \t: " << tampil << endl;
+        	hsl.close();
+            }
+              
+     else if (ambil == 'b'){
+     		cout << "\n=======Struk SPBU UAD=======" << endl;
+     		cout << "Nama Pelanggan : " << na << endl;
+			cout << "Total Liter \t: " << liter << endl;
+        	cout << "Jenis Bensin \t: " << "Pertamax" << endl;
+        	cout << "Total Harga \t: " << tampil << endl;
+        	ofstream hsl;
+        	hsl.open("struct.txt", ios::out);
+        	hsl <<  "\n=======Struk SPBU UAD=======" << endl;
+        	hsl <<  "Nama Pelanggan : " << na << endl;
+        	hsl <<  "Total Liter \t: " << liter << endl;
+        	hsl <<  "Jenis Bensin \t: " << "Pertamax" << endl;
+        	hsl <<  "Total Harga \t: " << tampil << endl;
+        	hsl.close();
+          }
+     system("PAUSE");
+     system("cls");
+     pilihan();
+     }
+int main(int argc, char *argv[])
+{
+    Bensin x;
+    x.pilihan();
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
